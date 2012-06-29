c1bf483b314205b52481cd37e9f10a35ae37d036
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2012-06-29 14:50:39 +0000

    kbat.content message factory defined

--- plone/kbat.content/kbat/content/__init__.py
+++ plone/kbat.content/kbat/content/__init__.py
@@ -1,5 +1,7 @@
 # -*- extra stuff goes here -*-
+from zope.i18nmessageid import MessageFactory
 
+_ = MessageFactory("kbat.content")
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
