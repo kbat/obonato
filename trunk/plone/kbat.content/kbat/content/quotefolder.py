4c744a1cc4a6224096d672745e9433eae941da02
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2012-06-29 20:17:16 +0000

    basic views for quote and quotefolder created

--- trunk/plone/kbat.content/kbat/content/quotefolder.py
+++ trunk/plone/kbat.content/kbat/content/quotefolder.py
@@ -4,6 +4,10 @@ from zope import schema
 from plone.directives import form, dexterity
 from plone.app.textfield import RichText
 
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from kbat.content.quote import IQuote
+
 from kbat.content import _
 
 class IQuoteFolder(form.Schema):
@@ -14,9 +18,29 @@ class IQuoteFolder(form.Schema):
         title=_(u"Title"),
         )
     
-    
     details = RichText(
         title=_(u"Text"),
         required=False
         )
 
+
+# View registration:
+# http://plone.org/products/dexterity/documentation/manual/developer-manual/custom-views/referencemanual-all-pages
+# the view name is @view taken from the class name in lowercase
+# this is associated with quotefolder_templates/view.pt
+class View(grok.View):
+    grok.context(IQuoteFolder)
+# permission is Zope3 permission called 'zope2.View'. Permission 'zope.Public' means 'no permission required'
+# list of other standard permissions are in parts/omelette/Products/Five/permissions.zcml
+    grok.require('zope2.View')
+    
+    def quotes(self):
+        """Return a catalog search result of quotes to show
+        """
+        
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        
+        return catalog(object_provides=IQuote.__identifier__,
+                       path='/'.join(context.getPhysicalPath()),
+                       sort_on='sortable_title')
