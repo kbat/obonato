72c9e683b5bbaa4cc48425a4d865f18e39873959
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2012-06-29 15:09:42 +0000

    quote.py added with initial schema

---
+++ trunk/plone/kbat.content/kbat/content/quote.py
@@ -0,0 +1,27 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+from plone.app.textfield import RichText
+
+class IQuote(form.Schema):
+    """A quote. Sessions are managed inside Quote Folder.
+    """
+    
+    title = schema.TextLine(
+        title=_(u"Title"),
+        )
+    
+    
+    details = RichText(
+        title=_(u"Text"),
+        required=False
+        )
+
+    title = schema.TextLine(
+        title=_(u"Author"),
+        )
+
+    url = schema.TextLine(
+        title=_(u"Link"),
+        )
