e29ada503d7fcbdd8f21046a430f22495846d50e
batkov@gmail.com <batkov@gmail.com@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2013-01-10 12:49:04 +0000

    featured items portlet added (yet empty)

--- trunk/plone/lizardie.content/lizardie/content/portlets/featured.py
+++ trunk/plone/lizardie.content/lizardie/content/portlets/featured.py
@@ -1,160 +1,99 @@
-"""Define a portlet used to show featured items. This follows the patterns from
-plone.app.portlets.portlets. Note that we also need a portlet.xml in the
-GenericSetup extension profile to tell Plone about our new portlet.
-See page 278.
-"""
+from zope.interface import Interface
+from zope.interface import implements
 
-import random
+from plone.app.portlets.portlets import base
+from plone.portlets.interfaces import IPortletDataProvider
 
 from zope import schema
 from zope.formlib import form
-from zope.interface import implements
-from zope.component import getMultiAdapter
-
-from plone.app.portlets.portlets import base
-from plone.memoize.instance import memoize
-from plone.portlets.interfaces import IPortletDataProvider
-
-from DateTime import DateTime
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from Products.CMFCore.utils import getToolByName
+from lizardie.content import _
 
-from lizardie.content.featured import IFeatured
-from lizardie.content import MessageFactory as _
+from zope.i18nmessageid import MessageFactory
+__ = MessageFactory("plone")
 
-# This interface defines the configurable options (if any) for the portlet.
-# It will be used to generate add and edit forms.
+class IFeatured(IPortletDataProvider):
+    """A portlet
 
-class IFeaturedItemsPortlet(IPortletDataProvider):
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
 
-    count = schema.Int(
-            title=_(u"Number of featured items to display"),
-            description=_(u"Maximum number of featured items to show"),
-            required=True,
-            default=5,
-        )
-                       
-    randomize = schema.Bool(
-            title=_(u"Randomize featured items"),
-            description=_(u"If enabled, featured items to show will"
-                            "be picked randomly. If disabled, newer "
-                            "items will be preferred."),
-            default=False,
-        )
-                            
-    sitewide = schema.Bool(
-            title=_(u"Sitewide items"),
-            description=_(u"If enabled, featured items from across the "
-                           "site will be found. If disabled, only "
-                           "items in this folder and its "
-                           "subfolders are eligible."),
-            default=False,
-        )
+    # TODO: Add any zope.schema fields here to capture portlet configuration
+    # information. Alternatively, if there are no settings, leave this as an
+    # empty interface - see also notes around the add form and edit form
+    # below.
 
-# The assignment is a persistent object used to store the configuration of
-# a particular instantiation of the portlet.
+    # some_field = schema.TextLine(title=_(u"Some field"),
+    #                              description=_(u"A field to use"),
+    #                              required=True)
+
 
 class Assignment(base.Assignment):
-    implements(IFeaturedItemsPortlet)
+    """Portlet assignment.
 
-    def __init__(self, count=5, randomize=False, sitewide=False):
-        self.count = count
-        self.randomize = randomize
-        self.sitewide = sitewide
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
 
-    title = _(u"Featured items") # defined in portlets.interfaces.IPortletAssignment, declared by base.Assignment. It is possible to use a dynamic @property for this, if required
+    implements(IFeatured)
 
-# The renderer is like a view (in fact, like a content provider/viewlet) exchept that it only renders a part of a page
-# The item self.data will typically be the assignment (although it is possible
-# that the assignment chooses to return a different object - see 
-# base.Assignment).
-# This class describes most of the logic of the portlet.
+    # TODO: Set default values for the configurable parameters here
+
+    # some_field = u""
+
+    # TODO: Add keyword parameters for configurable parameters here
+    # def __init__(self, some_field=u''):
+    #    self.some_field = some_field
+
+    def __init__(self):
+        pass
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        return __(u"Featured items")
+
 
 class Renderer(base.Renderer):
+    """Portlet renderer.
 
-    # render() will be called to render the portlet - see page 282
-    
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
     render = ViewPageTemplateFile('featured.pt')
-       
-    # The 'available' property is used to determine if the portlet should
-    # be shown.
-        
-    @property
-    def available(self): # see page 282 "Portlet renderers"
-        return len(self._data()) > 0
 
-    # To make the view template as simple as possible, we return dicts with
-    # only the necessary information.
 
-    def featured(self):
-        for brain in self._data():
-            fi = brain.getObject()
-            
-            scales = getMultiAdapter((fi, self.request), name='images')
-            scale = scales.scale('image', scale='thumb')
-            imageTag = None
-            if scale is not None:
-                imageTag = scale.tag()
-            
-            yield dict(title=fi.Title(),
-                       summary=fi.Description(),
-                       url=brain.getURL(),
-                       imageTag=imageTag)
-
-    # By using the @memoize decorator, the return value of the function will
-    # be cached. Thus, calling it again does not result in another query.
-    # See the plone.memoize package for more.
-        
-    @memoize
-    def _data(self):
-        limit = self.data.count
-        
-        query = dict(object_provides=IDoll.__identifier__)
-        
-        if not self.data.sitewide:
-            query['path'] = '/'.join(self.context.getPhysicalPath())
-        if not self.data.randomize:
-            query['sort_on'] = 'modified'
-            query['sort_order'] = 'reverse'
-            query['sort_limit'] = limit
-        
-        # Ensure that we only get active objects, even if the user would
-        # normally have the rights to view inactive objects (as an
-        # administrator would)
-        query['effectiveRange'] = DateTime()
-        
-        catalog = getToolByName(self.context, 'portal_catalog')
-        results = catalog(query)
-        
-        featured = []
-        if self.data.randomize:
-            featured = list(results)
-            featured.sort(lambda x,y: cmp(random.randint(0,200),100))
-            featured = featured[:limit]
-        else:
-            featured = results[:limit]
-        
-        return featured
-
-# Define the add forms and edit forms, based on zope.formlib. These use
-# the interface to determine which fields to render.
+# NOTE: If this portlet does not have any configurable parameters, you can
+# inherit from NullAddForm and remove the form_fields variable.
 
 class AddForm(base.AddForm):
-    form_fields = form.Fields(IFeaturedItemsPortlet)
-    label = _(u"Add Featured portlet")
-    description = _(u"This portlet displays featured items.")
+    """Portlet add form.
 
-    # This method must be implemented to actually construct the object.
-    # The 'data' parameter is a dictionary, containing the values entered
-    # by the user.
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(IFeatured)
 
     def create(self, data):
-        assignment = Assignment()
-        form.applyChanges(assignment, self.form_fields, data)
-        return assignment
+        return Assignment(**data)
+
+
+# NOTE: IF this portlet does not have any configurable parameters, you can
+# remove this class definition and delete the editview attribute from the
+# <plone:portlet /> registration in configure.zcml
 
 class EditForm(base.EditForm):
-    form_fields = form.Fields(IFeaturedItemsPortlet)
-    label = _(u"Edit Featured portlet")
-    description = _(u"This portlet displays featured items.")
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(IFeatured)
