988e701fb5e720267a152e7a03c3bf9db88d60d4
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2013-09-20 18:37:28 +0000

    irwilot.policy added

---
+++ plone/irwilot.policy/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='irwilot.policy',
+      version=version,
+      description="irwilot policy package",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['irwilot'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Products.PloneFormGen',
+          'irwilot.content',
+#          'irwilottheme.bootstrap',
+          'collective.prettyphoto', # for the pop-up effect
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
