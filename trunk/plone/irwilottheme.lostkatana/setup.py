111cfc4a89012fa95dbc3e5ba2c77a9516470227
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2013-10-06 14:59:10 +0000

    irwilottheme.lostkatana just created with '../bin/zopeskel plone'

---
+++ trunk/plone/irwilottheme.lostkatana/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='irwilottheme.lostkatana',
+      version=version,
+      description="beyondskins.lostkatana-inspired theme for irwilot",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['irwilottheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
