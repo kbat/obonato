e4288c10278bba7d9887d8717eea1fd05a694352
batkov <batkov@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2013-10-15 19:13:30 +0000

    description format changed

--- trunk/plone/irwilot.content/irwilot/content/illustration.py
+++ trunk/plone/irwilot.content/irwilot/content/illustration.py
@@ -150,10 +150,12 @@ class View(dexterity.DisplayForm):
         """
         self.birthdayFormatted = self.context.start.strftime("%d %b %Y")
         self.birthdayYear = self.context.start.year
-        self.dimensions = "%d x %d cm" % (self.context.height, self.context.width)
+        self.dimensions = "%d&nbsp;&times;&nbsp;%d&nbsp;cm" % (self.context.height, self.context.width)
 #        self.context.image = self.context.picture # to be able to treat illustration as image in TinyMCE
 #        self.context.picture = self.context.image # temporary before I fixed templates
         self.context.description = "%s  %s %s %s %d" % (self.dimensions, self.context.genre, self.context.style, self.context.technique, self.context.start.year)
+
+        self.context.description = "%s &bull; %s &bull; %d" % (self.context.technique, self.dimensions, self.context.start.year)
         
     def mainimage(self):
         """Return image to show in IllustrationFolder view
