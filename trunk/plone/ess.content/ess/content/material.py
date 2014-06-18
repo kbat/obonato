96cc22615ef7e4de17f07d8e88e18b193f248b69
batkov@gmail.com <batkov@gmail.com@111b92cd-1110-fce5-3fdb-b0c112430d0b>
2014-06-18 09:04:48 +0000

    material content types created

---
+++ trunk/plone/ess.content/ess/content/material.py
@@ -0,0 +1,102 @@
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+from ess.content import _
+#MessageFactory as _
+
+from zope.interface import implements
+from zope.interface import Interface
+
+from plone.app.content.interfaces import INameFromTitle
+from re import search # for validation
+
+class INameFromID(INameFromTitle):
+    def title():
+        """Return a processed material ID"""
+
+class NameFromID(object):
+    implements(INameFromID)
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def title(self):
+        return self.context.ID
+
+
+# Interface class; used to define content-type schema.
+
+class IMaterial(form.Schema):
+    """
+    Material for Monte Carlo studies
+    """
+
+    # title = schema.TextLine(
+    #     title=_(u"Title"),
+    #     required = True
+    #     )
+
+    ID = schema.ASCII(
+        title = _(u"Material ID"),
+        required = True,
+        )
+
+    mcnp_string = schema.ASCII(
+        title = _(u"MCNPX string"),
+        required = False,
+        )
+
+    mcnp_mt = schema.ASCIILine(
+        title = _(u"MCNPX MT"),
+        required = False,
+        )
+
+    mcnp_mx = schema.ASCIILine(
+        title = _(u"MCNPX MX"),
+        required = False,
+        )
+
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+
+class Material(Container):
+    grok.implements(IMaterial)
+
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# material_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class View(grok.View):
+    """ sample view class """
+
+    grok.context(IMaterial)
+    grok.require('zope2.View')
+
+    # grok.name('view')
+
+    # Add view methods here
