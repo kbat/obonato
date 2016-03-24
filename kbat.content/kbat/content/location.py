f8c497ffe51e70aababa95fed871e011c9b7857d
Konstantin Batkov <batkov@gmail.com>
2016-03-24 17:35:54 +0100

    first version of trips and location added

---
+++ kbat.content/kbat/content/location.py
@@ -0,0 +1,75 @@
+from five import grok
+from plone.directives import dexterity, form
+import datetime
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+from plone.app.content.interfaces import INameFromTitle
+from plone.memoize.instance import memoize
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+
+from kbat.content import _
+
+from zope.interface import implements
+from zope.interface import Interface
+
+
+class INameFromDate(INameFromTitle):
+    def title():
+        """Return a processed title"""
+
+class NameFromDate(object):
+    implements(INameFromDate)
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def title(self):
+        return self.context.start.strftime("%y%m%d")
+
+# Interface class; used to define content-type schema.
+
+class ILocation(form.Schema):
+    """
+    Location of the Blog
+    """
+    
+    # If you want a schema-defined interface, delete the form.model
+    # line below and delete the matching file in the models sub-directory.
+    # If you want a model-based interface, edit
+    # models/location.xml to define the content type
+    # and add directives here as necessary.
+    
+    start = schema.Date(
+        title = _(u"Date"),
+        required = True,
+        )
+
+class View(grok.View):
+    grok.context(ILocation)
+    grok.require('zope2.View')
+    
+    # grok.name('view')
+
+    def update(self):
+        self.dateFormatted = self.context.start.strftime("%d %b %Y")
+
+@form.default_value(field=ILocation['start'])
+def startDefaultValue(data):
+    return datetime.datetime.today()
---
+++ kbat.content/kbat/content/trips.py
@@ -0,0 +1,84 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid, alsoProvides
+try:
+    # plone 4
+    from zope.app.content.interfaces import IContentType
+except ImportError:
+    # plone 5
+    from plone.dexterity.interfaces import IContentType
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+#from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from kbat.content import MessageFactory as _
+
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from kbat.content.location import ILocation
+
+
+
+# Interface class; used to define content-type schema.
+
+class ITrips(form.Schema):
+    """
+    Trips - a folder for trips
+    """
+    
+    # If you want a schema-defined interface, delete the form.model
+    # line below and delete the matching file in the models sub-directory.
+    # If you want a model-based interface, edit
+    # models/Trips.xml to define the content type
+    # and add directives here as necessary.
+ 
+
+#    form.model("models/Trips.xml")
+#alsoProvides(ITrips, IContentType)
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+
+class Trips(dexterity.Container):
+    grok.implements(ITrips)
+    
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# Trips_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class View(grok.View):
+    grok.context(ITrips)
+    grok.require('zope2.View')
+    
+    # grok.name('view')
+
+    def locations(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+
+        return catalog(object_provides=ILocation.__identifier__,
+                       path='/'.join(context.getPhysicalPath()))
