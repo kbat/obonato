8952d4a9473853fec51c3b8ce20eca666412ac8c
Konstantin Batkov <batkov@gmail.com>
2020-04-30 15:47:29 +0200

    library default view lists components with lead images

--- cl.content/src/cl/content/content/library.py
+++ cl.content/src/cl/content/content/library.py
@@ -3,8 +3,9 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
-#from cl.content.content.component import IComponent
+from cl.content.content.component import IComponent
 from zope.interface import implementer
+from Acquisition import aq_inner
 
 class ILibrary(model.Schema):
     """ Marker interface for Library
@@ -22,10 +23,10 @@ class View(BrowserView):
     def components(self):
         """ Return catalogue search result of components """
 
-        context = eq_inner(self.context)
+        context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
-        a
+
         return catalog(object_provides=IComponent.__identifier__,
                        path='/'.join(context.getPhysicalPath()),
-                       sort_on='start',
+                       sort_on='sortable_title',
                        sort_order='descending')
