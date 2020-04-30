d106a7f0ad00ecf74671d84e8eacdfe41307b9bc
Konstantin Batkov <batkov@gmail.com>
2020-04-30 13:28:50 +0200

    library view return catalog of components

--- cl.content/src/cl/content/content/library.py
+++ cl.content/src/cl/content/content/library.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.content import Container
 from plone.supermodel import model
-
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+#from cl.content.content.component import IComponent
+from zope.interface import implementer
 
 class ILibrary(model.Schema):
     """ Marker interface for Library
     """
+
+@implementer(ILibrary)
+class Library(Container):
+    """
+    """
+
+class View(BrowserView):
+    """ Browser view class
+    """
+
+    def components(self):
+        """ Return catalogue search result of components """
+
+        context = eq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        a
+        return catalog(object_provides=IComponent.__identifier__,
+                       path='/'.join(context.getPhysicalPath()),
+                       sort_on='start',
+                       sort_order='descending')
