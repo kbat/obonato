900426febc18107156634a03507a4f5df0b13216
Konstantin Batkov <batkov@gmail.com>
2020-05-11 13:33:27 +0200

    simple constraints for headerURL

--- cl.content/src/cl/content/content/component.py
+++ cl.content/src/cl/content/content/component.py
@@ -13,9 +13,41 @@ from zope import schema
 from zope.interface import implementer
 #from collective import dexteritytextindexer
 from Products.Five import BrowserView
+from zope.interface import Invalid
+import urllib
 
 from cl.content import _
 
+origin = 'https://github.com/sansell/comblayer'
+
+def url_is_alive(url):
+    """Checks that a given URL is reachable.
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except urllib.request.HTTPError:
+        return False
+
+def headerURL_constraint(value):
+    """Check that the headerURL is OK
+    """
+    if not value.endswith('.h'):
+        raise Invalid(_(u"Header file extension must end with .h"))
+    if not value.lower().startswith(origin+'/'):
+        raise Invalid(_(u'URL must start with %s' % origin))
+
+    ### this sends too many requests -> commenting out
+    # response = urllib.urlopen(value)
+    # code = response.getcode()
+    # response.close()
+    # if code == 404:
+    #     raise Invalid(_(u'Address is not reachable'))
+
+    return True
 
 class IComponent(model.Schema):
     """ Marker interface and Dexterity Python Schema for Component
@@ -36,6 +68,7 @@ class IComponent(model.Schema):
     headerURL = schema.URI(
         title=_(u'Header URL'),
         description=_('Link to the class header on GitHub'),
+        constraint = headerURL_constraint,
         required=True
     )
 
